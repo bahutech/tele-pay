d7c945351a6b7fe7a03837970a7ab71f25a81783
bahutech <54044336+bahutech@users.noreply.github.com>
2024-10-22 23:16:23 +0300

    Update applyFabricTokenService.js

--- service/applyFabricTokenService.js
+++ service/applyFabricTokenService.js
@@ -12,22 +12,23 @@ function applyFabricToken() {
         "Content-Type": "application/json",
         "X-APP-Key": config.fabricAppId,
       },
-      rejectUnauthorized: false, //add when working with https sites
-      requestCert: false, //add when working with https sites
-      agent: false, //add when working with https sites
+      rejectUnauthorized: true, //add when working with https sites
+      requestCert: true, //add when working with https sites
+      agent: true, //add when working with https sites
       body: JSON.stringify({
         appSecret: config.appSecret,
       }),
     };
+    console.log("*****applyFabricToken  options******");
     console.log(options);
     request(options, function (error, response) {
+      console.log("***********");
       if (error) throw new Error(error);
-      // console.log("***********");
       console.log("BODY", response.body);
       // console.log(typeof response.body);
       let result = JSON.parse(response.body);
-      // console.log(result);
-      // console.log("*****************");
+      console.log(result);
+      console.log("*****************");
       resolve(result);
     });
   });
